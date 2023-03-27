fef407fb7f81531e2f5b18bb50783d7917a9c941
arthursfares <arthursfares@gmail.com>
2023-03-27 00:17:45 -0300

    load ML models before application starts up

--- api/main.py
+++ api/main.py
@@ -1,6 +1,20 @@
+from contextlib import asynccontextmanager
+
+import tensorflow as tf
+import numpy
 from fastapi import FastAPI
 
-app = FastAPI()
+
+ml_models = {}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    ml_models["style_predict"] = tf.keras.utils.get_file('style_predict.tflite', 'https://tfhub.dev/google/lite-model/magenta/arbitrary-image-stylization-v1-256/int8/prediction/1?lite-format=tflite')
+    ml_models["style_transform"] = tf.keras.utils.get_file('style_transform.tflite', 'https://tfhub.dev/google/lite-model/magenta/arbitrary-image-stylization-v1-256/int8/transfer/1?lite-format=tflite')
+    yield
+    ml_models.clear()
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
